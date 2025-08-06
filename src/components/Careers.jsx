64f59f39c5c77a6ea1e5e98adb791b7950422570
Sneha Ruth G <sneharuth123@gmail.com>
2025-08-06 18:42:52 +0530

    Update Careers.jsx

--- src/components/Careers.jsx
+++ src/components/Careers.jsx
@@ -4,7 +4,7 @@ function Careers() {
   return (
     <section className="p-10 bg-gray-100 text-center">
       <h2 className="text-2xl font-bold">Careers</h2>
-      <p>This is the Careers section.</p>
+      <p>This is the Careers section. we are hiring!!!</p>
     </section>
   );
 }
